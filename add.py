4837958218306d325ee22e48007b9259bab5afab
tharika <tharikashree12@gmail.com>
2026-03-23 10:56:41 +0530

    Initial commit

---
+++ add.py
@@ -0,0 +1,9 @@
+a=30
+b=40
+print("addition", a+b)
+print("subtraction", a-b)
+print("multiplication", a*b)
+print("division", a/b)
+print("modulus", a%b)
+print("floordivision", a//b)
+print("exponent", a**b)
